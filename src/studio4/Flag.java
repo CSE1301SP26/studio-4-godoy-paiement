b6a134b51db7316eaae8e3ae6b0afdd33ccd7b3e
rebeccagodoy <godoy.r@wustl.edu>
2026-02-18 15:38:15 -0600

    Studio 4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+
+		StdDraw.clear();
+		StdDraw.setXscale(0,22);
+		StdDraw.setYscale(0,16);
+		StdDraw.setPenColor(255,200,0);
+		StdDraw.filledRectangle(11,8,11,8);
+		StdDraw.setPenColor(255,175,175);
+		StdDraw.filledCircle(11, 8, 8);
+		StdDraw.setPenColor(128,0,128);
+		StdDraw.filledSquare(1.5,14.5,1);
+		StdDraw.filledSquare(1.5,1.5,1);
+		StdDraw.filledSquare(20.5,14.5,1);
+		StdDraw.filledSquare(20.5,1.5,1);
+	
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -8,6 +8,7 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
@@ -19,6 +20,39 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		StdDraw.setPenColor(Color.GREEN);
+		if (shapeType.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if (shapeType.equals("ellipse")) {
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		
+			
+
+		}
+		
+
+
 		
 	}
-}
+
